a96f364097936fa8a84c479d8009e47bd5050b0a
yanisdxw <yanisdxw@gmail.com>
2019-08-20 22:38:40 +0800

    rpc

--- service-1/src/main/java/com/dxw/cloud/Server1Application.java
+++ service-1/src/main/java/com/dxw/cloud/Server1Application.java
@@ -1,10 +1,12 @@
 package com.dxw.cloud;
 
+import io.ostenant.rpc.thrift.server.annotation.EnableThriftServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableThriftServer
 @EnableDiscoveryClient
 public class Server1Application {
 
